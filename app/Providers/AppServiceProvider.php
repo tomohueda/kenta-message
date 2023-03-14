5eb12cd0d5662ce52e35e921297b413b90943e2f
Your Name <you@example.com>
2023-03-14 18:40:05 +0900

    LaravelとMySQLの問題

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        //Schema::defaultStringLength(191);
+        Schema::defaultStringLength(191);
     }
 }
