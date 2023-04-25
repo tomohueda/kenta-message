965e68051a5ef03ad99cc62cbedcea84f6ded212
Tomoh Ueda <tomohueda@gmail.com>
2023-04-25 15:09:56 +0900

    会社名修正

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -18,35 +18,35 @@ class MessageController extends Controller
 
         switch ($num) {
             case 1:
-                $title = "アブレイズPR";
+                $title = "ABLAZE(アブレイズ) PR";
                 $path = "data/1_ablaze.m4a";
                 break;
             case 2:
-                $title = "セトウチトノコヤPR";
+                $title = "セトウチトノコヤ PR";
                 $path = "data/2_tonokoya.m4a";
                 break;
             case 3:
-                $title = "ニシカイチPR";
+                $title = "株式会社ニシカイチ PR";
                 $path = "data/3_nishikaichi.m4a";
                 break;
             case 4:
-                $title = "フレスタPR";
+                $title = "フレスタ PR";
                 $path = "data/4_fresta.m4a";
                 break;
             case 5:
-                $title = "丸子ガラスPR";
+                $title = "丸子硝子株式会社 PR";
                 $path = "data/5_maruko.m4a";
                 break;
             case 6:
-                $title = "広栄水産PR";
+                $title = "広栄水産 PR";
                 $path = "data/6_suisan.m4a";
                 break;
             case 7:
-                $title = "寿屋PR";
+                $title = "寿屋珈琲 PR";
                 $path = "data/7_kotobukiya.m4a";
                 break;
             case 8:
-                $title = "小麦屋PR";
+                $title = "小麦屋 PR";
                 $path = "data/8_komugiya.m4a";
                 break;
         }
